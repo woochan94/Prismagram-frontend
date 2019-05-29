c6a96b22e688eefea71a1f1265ba46447fcaf358
정우찬 <vdncks25@naver.com>
2019-05-29 17:31:42 +0900

    FEAT : Apply globalStyles and set Theme
    globalstyle 적용 및 theme 세팅 
    - react-toastify 설치 
    - styled-reset 설치 
    -

--- prismagram-frontend/src/Components/App.js
+++ prismagram-frontend/src/Components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from '../Styles/Theme';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+export default () => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+  </ThemeProvider>
+)
 
-export default App;
---
+++ prismagram-frontend/src/Styles/Theme.js
@@ -0,0 +1,20 @@
+const boxBorder = "1px solid #e6e6e6"; 
+const borderRadius = "4px";
+
+
+// Styled components에서 변수처럼 사용함 
+export default {
+    bgColor: "#FAFAFA", 
+    blackColor: "#262626",
+    darkGreyColor: "#999", 
+    lightGreyColor: "#c7c7c7",
+    redColor: "#ED4956",   
+    blueColor: "#3897f0",
+    darkBlueColor: "#003569",
+    boxBorder, 
+    borderRadius, 
+    whiteBox: `${borderRadius}; 
+                ${boxBorder};
+                backgrond-color: white;
+            `
+}
