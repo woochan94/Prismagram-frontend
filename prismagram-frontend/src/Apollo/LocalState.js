dd29df54eb940c0c2fb3356917713b695c17b65d
정우찬 <vdncks25@naver.com>
2019-05-29 20:51:08 +0900

    FEAT : Make hooks query
    gql을 이용하여 query 생성

--- prismagram-frontend/src/Apollo/LocalState.js
+++ prismagram-frontend/src/Apollo/LocalState.js
@@ -1,6 +1,6 @@
 // Local State는 기본적으로 Client에 없는 state이다. 
 export const defaults = {
-    isLoggedIn: localStorage.getItem("token") !== null ? true : false 
+    isLoggedIn: Boolean(localStorage.getItem("token")) || false
 }
 
 export const resolvers = {
