d75cc0fe0fdc182f46b9122c20fa9825154cd02b
정우찬 <vdncks25@naver.com>
2019-05-30 16:38:29 +0900

    FEAT : Add toastify and createAccount mutation
    toast와 같은 메시지 창을 사용 
    createAccount mutation 생성

--- prismagram-frontend/src/Components/App.js
+++ prismagram-frontend/src/Components/App.js
@@ -1,37 +1,39 @@
-import React from 'react';
+import React from "react";
 import { gql } from "apollo-boost";
 import styled, { ThemeProvider } from "styled-components";
+import { useQuery } from "react-apollo-hooks";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import GlobalStyles from "../Styles/GlobalStyles";
-import Theme from '../Styles/Theme';
-import AppRouter from './Router';
-import { useQuery } from 'react-apollo-hooks';
-import Footer from './Footer';
+import Theme from "../Styles/Theme";
+import Router from "./Router";
+import Footer from "./Footer";
 
 const QUERY = gql`
   {
-    isLoggedIn @client 
+    isLoggedIn @client
   }
 `;
 
 const Wrapper = styled.div`
-  margin: 0 auto; 
-  max-width: 935px; 
-  width: 100%; 
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%;
 `;
 
 export default () => {
-  const { 
-    data: { isLoggedIn } 
-  } = useQuery(QUERY); 
-  
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+
   return (
     <ThemeProvider theme={Theme}>
       <Wrapper>
         <GlobalStyles />
-        <AppRouter isLoggedIn={isLoggedIn} />
+        <Router isLoggedIn={isLoggedIn} />
         <Footer />
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
       </Wrapper>
     </ThemeProvider>
-  )
-}
-
+  );
+};
