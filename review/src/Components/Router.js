d35a4990dffdb25e1db6aa6633d17ba66ca8b517
정우찬 <vdncks25@naver.com>
2019-06-08 15:41:11 +0900

    REVIEW : #4.2 React Router

---
+++ review/src/Components/Router.js
@@ -0,0 +1,31 @@
+// Router Component는 어떤 라우터를 보여줄지 다루는 Component이다. 
+import React from "react";
+import PropTypes from "prop-types";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Feed from "../Routes/Feed";
+import Auth from "../Routes/Auth";
+
+const LoggedInRoutes = () => (
+    <>
+        <Route exact path="/" component={Feed} />
+    </>
+);
+
+const LoggedOutRoutes = () => (
+    <>
+        <Route exact path="/" component={Auth} />
+    </>
+);
+
+const AppRouter = ({ isLoggedIn }) => (
+    <Router>
+        <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</Switch>
+    </Router>
+);
+
+// Router는 항상 proptypes를 가져야 한다. 
+AppRouter.propTypes = {
+    isLoggedIn : PropTypes.bool.isRequired
+}
+
+export default AppRouter;
