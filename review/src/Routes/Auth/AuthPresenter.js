77b55c3c9d73dabe1bfc0e660dd33d4d89d88f84
정우찬 <vdncks25@naver.com>
2019-06-09 20:51:24 +0900

    REVIEW : #5.8 confirmSecret + Log In Mutation

--- review/src/Routes/Auth/AuthPresenter.js
+++ review/src/Routes/Auth/AuthPresenter.js
@@ -1,5 +1,5 @@
-import React from "react"; 
-import styled from "styled-components"; 
+import React from "react";
+import styled from "styled-components";
 import Input from "../../Components/Input";
 import Button from "../../Components/Button";
 
@@ -47,51 +47,56 @@ const Form = styled(Box)`
 `;
 
 export default ({
-    action, 
+    action,
     setAction,
     username,
     firstName,
     lastName,
-    email, 
+    email,
     onSubmit,
     secret
 }) => {
     return (
         <Wrapper>
             <Form>
-                {action === "logIn" &&
-                    (<form onSubmit={onSubmit}>
+                {action === "logIn" && (
+                    <form onSubmit={onSubmit}>
                         <Input placeholder={"Email"} {...email} type="email" />
                         <Button text={"Log in"} />
-                    </form>)
-                }
-                {action === "signUp" &&
-                    (<form onSubmit={onSubmit}>
+                    </form>
+                )}
+                {action === "signUp" && (
+                    <form onSubmit={onSubmit}>
                         <Input placeholder={"First name"} {...firstName} />
                         <Input placeholder={"Last name"} {...lastName} />
                         <Input placeholder={"Email"} {...email} type="email" />
                         <Input placeholder={"Username"} {...username} />
                         <Button text={"Sign up"} />
-                    </form>)
-                } 
-                {action === "confirm" && 
-                    (<form onSubmit={onSubmit}>
-                        <Input placeholder={"Paste your secret"} required {...secret} />
+                    </form>
+                )}
+                {action === "confirm" && (
+                    <form onSubmit={onSubmit}>
+                        <Input placeholder="Paste your secret" required {...secret} />
                         <Button text={"Confirm"} />
-                    </form>)
-                }
+                    </form>
+                )}
             </Form>
-            <StateChanger>
-                {action === "logIn" ? 
-                (<>
-                    Don't have an account?{""}
-                    <Link onClick={()=> setAction("signUp")}>Sign Up</Link>
-                </>) :
-                (<>
-                    Have an account?{""}
-                    <Link onClick={() => setAction("logIn")}>Log in</Link>
-                </>)}
-            </StateChanger>
+
+            {action !== "confirm" && (
+                <StateChanger>
+                    {action === "logIn" ? (
+                        <>
+                            Don't have an account?{" "}
+                            <Link onClick={() => setAction("signUp")}>Sign up</Link>
+                        </>
+                    ) : (
+                            <>
+                                Have an account?{" "}
+                                <Link onClick={() => setAction("logIn")}>Log in</Link>
+                            </>
+                        )}
+                </StateChanger>
+            )}
         </Wrapper>
     );
 }
