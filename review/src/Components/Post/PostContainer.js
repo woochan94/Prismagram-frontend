cb05aa8ce8b0eef5a814a941143651ba8dd06590
정우찬 <vdncks25@naver.com>
2019-06-10 21:12:23 +0900

    REVIEW : #7.3 Post Component part Three

--- review/src/Components/Post/PostContainer.js
+++ review/src/Components/Post/PostContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import PostPresenter from "./PostPresenter";
 import useInput from "../../Hooks/useInput"; 
@@ -16,7 +16,21 @@ const PostContainer = ({
 }) => {
   const [isLikedS, setIsLiked] = useState(isLiked);
   const [likeCountS, setLikeCount] = useState(likeCount);
+  const [currentItem, setCurrentItem] = useState(0);
   const comment = useInput("");
+  const slide = () => {
+    const totalFiles = files.length; 
+    if(currentItem === totalFiles - 1) {
+      setTimeout(() => setCurrentItem(0), 3000); 
+    } else {
+      setTimeout(() => setCurrentItem(currentItem + 1), 3000);
+    }
+  };
+  useEffect(() => {
+    slide();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentItem]);
+
   return (
     <PostPresenter
       id={id}
@@ -31,6 +45,7 @@ const PostContainer = ({
       newComment={comment}
       setIsLiked={setIsLiked}
       setLikeCount={setLikeCount}
+      currentItem={currentItem}
     />
   )
 }
--- review/src/Components/Post/PostPresenter.js
+++ review/src/Components/Post/PostPresenter.js
@@ -1,45 +1,62 @@
-import React from "react"; 
-import styled from "styled-components"; 
-import Avatar from "../Avatar";
+import React from "react";
+import styled from "styled-components";
+import TextareaAutosize from "react-autosize-textarea";
 import FatText from "../FatText";
-import { HeartFull, HeartEmpty, Comment } from './../Icons';
-
+import Avatar from "../Avatar";
+import { HeartFull, HeartEmpty, Comment } from "../Icons";
 
 const Post = styled.div`
-    ${props => props.theme.whiteBox};
-    width: 100%;
-    max-width: 600px;
-    margin-bottom: 25px;
+  ${props => props.theme.whiteBox};
+  width: 100%;
+  max-width: 600px;
+  margin-bottom: 25px;
 `;
 
 const Header = styled.header`
-    padding: 15px;
-    display: flex; 
-    align-items: center;
-`; 
-
-const UserColumn = styled.div`
-    margin-left: 10px;
-`; 
-
-const Location = styled.span`
-    display: block;
-    margin-top: 5px;
-    font-size: 12px;
+  padding: 15px;
+  display: flex;
+  align-items: center;
 `;
 
-const Files = styled.div``; 
+const UserColumn = styled.div`
+  margin-left: 10px;
+`;
 
-const File = styled.img`
-    max-width: 100%;
-`; 
+const Location = styled.span`
+  display: block;
+  margin-top: 5px;
+  font-size: 12px;
+`;
+
+const Files = styled.div`
+  position: relative;
+  padding-bottom: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  flex-shrink: 0;
+`;
+
+const File = styled.div`
+  max-width: 100%;
+  width: 100%;
+  height: 600px;
+  position: absolute;
+  top: 0;
+  background-image: url(${props => props.src});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  opacity: ${props => (props.showing ? 1 : 0)};
+  transition: opacity 0.5s linear;
+`;
 
 const Button = styled.span`
-    cursor:pointer;
+  cursor: pointer;
 `;
 
 const Meta = styled.div`
-    padding: 15px;
+  padding: 15px;
 `;
 
 const Buttons = styled.div`
@@ -62,32 +79,56 @@ const Timestamp = styled.span`
   border-bottom: ${props => props.theme.lightGreyColor} 1px solid;
 `;
 
+const Textarea = styled(TextareaAutosize)`
+  border: none;
+  width: 100%;
+  resize: none;
+  font-size: 14px;
+  &:focus {
+    outline: none;
+  }
+`;
+
 export default ({
-    user: {username, avatar }, 
-    location,
-    files,
-    isLiked,
-    likeCount,
-    createdAt
+  user: { username, avatar },
+  location,
+  files,
+  isLiked,
+  likeCount,
+  createdAt,
+  newComment,
+  currentItem
 }) => (
-    <Post>
-        <Header>
-            <Avatar size="sm" url={avatar} />
-            <UserColumn>
-                <FatText text={username} />
-                <Location>{location}</Location>
-            </UserColumn>
-        </Header>
-        <Files>
-            {files && files.map(file => <File key={file.id} id={file.id} src={file.url} /> )}
-        </Files>
-        <Meta>
-            <Buttons>
-                <Button>{isLiked ? <HeartFull /> : <HeartEmpty />}</Button>
-                <Button><Comment /></Button>
-            </Buttons>
-            <FatText text={likeCount === 1 ? "1 like" : `${likeCount} likes`} />
-            <Timestamp>{createdAt}</Timestamp>
-        </Meta>
-    </Post>
+  <Post>
+    <Header>
+      <Avatar size="sm" url={avatar} />
+      <UserColumn>
+        <FatText text={username} />
+        <Location>{location}</Location>
+      </UserColumn>
+    </Header>
+            <Files>
+                {files &&
+                    files.map((file, index) => (
+                        <File
+                            key={file.id}
+                            id={file.id}
+                            src={file.url}
+                            // 해당 사진의 opacity만 1로 주기 CSS props
+                            showing={index === currentItem}
+                        />
+                    ))}
+            </Files>
+    <Meta>
+      <Buttons>
+        <Button>{isLiked ? <HeartFull /> : <HeartEmpty />}</Button>
+        <Button>
+          <Comment />
+        </Button>
+      </Buttons>
+      <FatText text={likeCount === 1 ? "1 like" : `${likeCount} likes`} />
+      <Timestamp>{createdAt}</Timestamp>
+      <Textarea placeholder={"Add a comment..."} {...newComment} />
+    </Meta>
+  </Post>
 );
