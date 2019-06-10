02d41ff0637f0e47c782e415d5e222be8749b86f
정우찬 <vdncks25@naver.com>
2019-06-10 17:21:25 +0900

    REVIEW : #7.1 Post Component part One

--- review/src/Routes/Feed/FeedPresenter.js
+++ review/src/Routes/Feed/FeedPresenter.js
@@ -1,6 +1,7 @@
 import React from "react"; 
 import styled from "styled-components"; 
 import Loader from "../../Components/Loader";
+import Post from "../../Components/Post"; 
 
 const Wrapper = styled.div`
   display: flex;
@@ -11,8 +12,23 @@ const Wrapper = styled.div`
 
 export default ({ data, loading }) => {
     return (
-        <Wrapper>
-            { loading && <Loader />}
-        </Wrapper>
-    )
-}
+      <Wrapper>
+        {loading && <Loader />}
+        {!loading &&
+          data &&
+          data.seeFeed &&
+          data.seeFeed.map(post => (
+            <Post
+              key={post.id}
+              id={post.id}
+              user={post.user}
+              files={post.files}
+              likeCount={post.likeCount}
+              isLiked={post.isLiked}
+              comments={post.comments}
+              createdAt={post.createdAt}
+            />
+          ))}
+      </Wrapper>
+    );
+  };
