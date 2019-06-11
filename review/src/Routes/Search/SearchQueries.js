efe3c94fc2ad7969e5dad77b11325ca3e21f4160
정우찬 <vdncks25@naver.com>
2019-06-11 17:17:22 +0900

    REVIEW : #8.4 SquarePost Component

--- review/src/Routes/Search/SearchQueries.js
+++ review/src/Routes/Search/SearchQueries.js
@@ -3,11 +3,13 @@ import { gql } from "apollo-boost";
 export const SEARCH = gql`
   query search($term: String!) {
     searchPost(term: $term) {
+      id
       files {
         id
         url
       }
       likeCount
+      commentCount
     }
     searchUser(term: $term) {
       id
